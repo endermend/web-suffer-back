23852764b91969d226d5474e65e2445e4405d4c7
endermend <endermend05@gmail.com>
2026-06-16 14:23:50 +1000

    Add CORS

--- backend/src/web_suffer/presentation/api/setup_middleware.py
+++ backend/src/web_suffer/presentation/api/setup_middleware.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from web_suffer.presentation.api.middleware.logging_middleware import LoggingMiddleware
 
@@ -6,3 +7,10 @@ from web_suffer.presentation.api.middleware.logging_middleware import LoggingMid
 def setup_middleware(app: FastAPI) -> None:
     """Подключение Middleware к FastAPI."""
     app.add_middleware(LoggingMiddleware)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:5173"],
+        allow_credentials=True,
+        allow_methods=["GET", "POST", "OPTIONS"],
+        allow_headers=["*"],
+    )
